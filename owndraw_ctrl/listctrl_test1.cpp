1cf9c6373f166c73e30e9dea0e63c975566164a9
2015 <2015@2011-2YY>
2017-05-25 20:59:00 +0800

    多个组，主题，排序。

--- owndraw_ctrl/listctrl_test1.cpp
+++ owndraw_ctrl/listctrl_test1.cpp
@@ -7,10 +7,38 @@
 
 BEGIN_MESSAGE_MAP(CMyListCtrl1, CListCtrl)
 	ON_NOTIFY_REFLECT(LVN_ENDLABELEDIT, &CMyListCtrl1::OnLvnEndlabeledit)
-	ON_WM_MEASUREITEM_REFLECT()
-	
-	ON_NOTIFY(HDN_TRACKW, 0, &CMyListCtrl1::OnHdnTrack)
+	ON_NOTIFY_REFLECT(LVN_COLUMNCLICK, &CMyListCtrl1::OnLvnColumnclick)
 END_MESSAGE_MAP()
+
+
+
+namespace {
+	//enum SORTTYPE {
+	//	STRING,
+	//	INTEGER,
+	//	PERCENTAGE
+	//};
+
+	//struct PARAMSORT {//排序参数
+	//	PARAMSORT(CListCtrl* pListCtrl, int nCol, bool bAscending, SORTTYPE t)
+	//		:pl(pListCtrl)
+	//		, col(nCol)
+	//		, asc(bAscending)
+	//		, type(t)
+	//	{}
+
+	//	CListCtrl* pl;
+	//	int  col;
+	//	bool asc;
+	//	SORTTYPE type;
+	//};
+
+}
+
+//排序函数
+//LPARAM LVITEM ，Ex是item的索引  
+int CALLBACK SortFunc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort);
+
 
 CMyListCtrl1::CMyListCtrl1()
 {
@@ -20,7 +48,7 @@ CMyListCtrl1::CMyListCtrl1()
 bool CMyListCtrl1::Init()
 {
 	//组
-	CreateSingleGroup(0, 0, _T("甲组"));
+	CreateGroup(0, 0, _T("甲组"));
 
 	return true;
 }
@@ -32,12 +60,6 @@ bool CMyListCtrl1::regex_callback(const boost::smatch& what)
 	return true;
 }
 
-BOOL CMyListCtrl1::PreCreateWindow(CREATESTRUCT& cs)
-{
-	// TODO: Add your specialized code here and/or call the base class
-
-	return __super::PreCreateWindow(cs);
-}
 
 
 void CMyListCtrl1::PreSubclassWindow()
@@ -45,56 +67,26 @@ void CMyListCtrl1::PreSubclassWindow()
 	// TODO: Add your specialized code here and/or call the base class
 	ModifyStyle(0, LVS_REPORT);
 	//ModifyStyleEx(0, LVS_EX_FULLROWSELECT | LVS_EX_DOUBLEBUFFER);
-	SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_DOUBLEBUFFER | LVS_EX_HEADERDRAGDROP
-		| LVS_EX_COLUMNSNAPPOINTS);
-	
-	bool b = IsCommonControlsEnabled();
+	SetExtendedStyle(GetExtendedStyle() | LVS_EX_FULLROWSELECT 
+		| LVS_EX_DOUBLEBUFFER | LVS_EX_HEADERDRAGDROP
+		| LVS_EX_COLUMNSNAPPOINTS/*最小宽度*/ | LVS_EX_CHECKBOXES
+		);
 
+	//bool b = IsCommonControlsEnabled();
+	//应用主题
+	EnableWindowTheme(GetSafeHwnd(), _T("ListView"), _T("Explorer"), NULL);
 
-	//SendMessage(WM_WINDOWPOSCHANGED, 0, 0);
-
-	//SetBkColor(RGB(0, 255, 0));
-	//CImageList* pImgList = GetImageList(LVSIL_NORMAL);
-	m_imgList.Create(32, 32, ILC_COLOR24, 0, 5);
+	m_imgList.Create(32, 32, ILC_COLOR24, 0, 10);
 	CBitmap bmp;
 	bmp.LoadBitmap(IDB_listctrl1);
 	m_imgList.Add(&bmp, RGB(0, 0, 0));
 	SetImageList(&m_imgList, LVSIL_NORMAL);
 
-	//int cx, cy;
-	//GetItemSpacing(FALSE, &cx, &cy);
-	//CSize sz(32+10, cy-10);
-	//SetIconSpacing(sz);
-
-//	LVITEM item;
-//	typedef struct tagLVITEMW
-//	{
-//		UINT mask;//LVIF_COLFMT(piColFmt,cColumns),LVIF_COLUMNS(cColumns),...
-//		int iItem;//item索引
-//		int iSubItem;//report
-//		UINT state;//被覆盖，被拖动，被剪切等，特复杂
-//		UINT stateMask;//哪些state可检索
-//		LPWSTR pszText;
-//		int cchTextMax;
-//		int iImage;
-//		LPARAM lParam;
-//		int iIndent;//缩进以image宽为单位
-//#if (NTDDI_VERSION >= NTDDI_WINXP)
-//		int iGroupId;
-//		UINT cColumns; // 数据列tile view columns
-//		PUINT puColumns;
-//#endif
-//#if (NTDDI_VERSION >= NTDDI_VISTA)
-//		int* piColFmt;
-//		int iGroup; // readonly. only valid for owner data.
-//#endif
-//	} LVITEMW, *LPLVITEMW;
-
-	//int nImgCnt = m_imgList.GetImageCount();
-	//for (int i = 0; i < nImgCnt; ++i) {
-	//	InsertItem(i, _T("animal"), i);
-	//}
-
+	//m_imgListGroup.Create(16, 16, ILC_COLOR24, 0, 10);
+	//CBitmap bmpGroup;
+	//bmpGroup.LoadBitmap(IDB_listctrl1_small);
+	//m_imgListGroup.Add(&bmpGroup, RGB(0, 0, 0));
+	//SetImageList(&m_imgListGroup, LVSIL_GROUPHEADER);
 
 	__super::PreSubclassWindow();
 }
@@ -117,37 +109,22 @@ void CMyListCtrl1::OnLvnEndlabeledit(NMHDR *pNMHDR, LRESULT *pResult)
 
 
 
-
-
-
-
-void CMyListCtrl1::MeasureItem(/*int nIDCtl, */LPMEASUREITEMSTRUCT lpMeasureItemStruct)
-{
-	// TODO: Add your message handler code here and/or call default
-	/*当 combo box , list box, list-view,menu item 创建时，发送给owner window
-	*/
-	lpMeasureItemStruct->itemHeight = 70;
-
-	//__super::OnMeasureItem(nIDCtl, lpMeasureItemStruct);
-}
-
-
-LRESULT CMyListCtrl1::CreateSingleGroup(int nIndex,
+LRESULT CMyListCtrl1::CreateGroup(int nIndex,
 	int nGroupId, CString strHeader)
 {
 	SetRedraw(FALSE);
 
 	//列
 	CHeaderCtrl* pHeader = GetHeaderCtrl();
-	CString strCols[] = { _T("姓名"),_T("年龄"), _T("性别"), _T("爱好"), _T("工作进度") };
 	LVCOLUMN lvCol;
 	lvCol.mask = LVCF_TEXT | LVCF_WIDTH | LVCF_SUBITEM | LVCF_MINWIDTH;
 	//lvCol.fmt = LVCFMT_LEFT;
 
 	int colWidthSum = 0, colWidth = 0;
-	int len = sizeof(strCols) / sizeof(strCols[0]);
+	//int len = sizeof(g_strCols) / sizeof(g_strCols[0]);
+	int len = m_vecColsName.size();
 	for (int i = 0; i < len; ++i) {
-		CString& str = strCols[i];
+		CString& str = m_vecColsName[i];
 
 		lvCol.pszText = str.GetBuffer();
 		lvCol.cchTextMax = str.GetLength();
@@ -164,19 +141,30 @@ LRESULT CMyListCtrl1::CreateSingleGroup(int nIndex,
 	//SetColumnWidth(len - 1, rc.right - colWidthSum + colWidth);
 	//禁止列拖动
 	//GetHeaderCtrl()->EnableWindow(FALSE);
-	//设置第一列排序小箭头
-	HDITEM hditem = { 0 };
-	hditem.mask = HDI_FORMAT;
-	pHeader->GetItem(0, &hditem);
-	hditem.fmt |= HDF_SORTDOWN;
-	pHeader->SetItem(0, &hditem);
+	//设置第一列排序小箭头升序
+	//HDITEM hditem = { 0 };
+	//hditem.mask = HDI_FORMAT;
+	//pHeader->GetItem(0, &hditem);
+	//hditem.fmt |= HDF_SORTUP;
+	//pHeader->SetItem(0, &hditem);
+
 
 	//item
 	//_T("姓名"),_T("年龄"), _T("性别"), _T("爱好"), _T("工作进度")
 	std::vector<std::vector<CString>> vec = {
 		{ _T("a"),_T("43"), _T("男"), _T("篮球"), _T("90%") },
-		{ _T("b"),_T("12"), _T("男"), _T("足球"), _T("90%") },
-		{ _T("c"),_T("3"), _T("女"), _T("篮球"), _T("90%") },
+		{ _T("b"),_T("12"), _T("男"), _T("足球"), _T("10%") },
+		{ _T("c"),_T("3"), _T("女"), _T("篮球"), _T("2%") },
+		{ _T("d"),_T("4"), _T("女"), _T("篮球"), _T("30%") },
+		{ _T("e"),_T("56"), _T("男"), _T("篮球"), _T("90%") },
+		{ _T("f"),_T("12"), _T("女"), _T("羽毛球"), _T("20%") },
+		{ _T("g"),_T("32"), _T("女"), _T("保龄球"), _T("90%") },
+		{ _T("h"),_T("22"), _T("男"), _T("高尔夫球"), _T("90%") },
+		{ _T("i"),_T("25"), _T("男"), _T("篮球"), _T("90%") },
+		{ _T("j"),_T("26"), _T("男"), _T("篮球"), _T("90%") },
+		{ _T("a"),_T("43"), _T("男"), _T("篮球"), _T("90%") },
+		{ _T("b"),_T("12"), _T("男"), _T("足球"), _T("10%") },
+		{ _T("c"),_T("3"), _T("女"), _T("篮球"), _T("2%") },
 		{ _T("d"),_T("4"), _T("女"), _T("篮球"), _T("30%") },
 		{ _T("e"),_T("56"), _T("男"), _T("篮球"), _T("90%") },
 		{ _T("f"),_T("12"), _T("女"), _T("羽毛球"), _T("20%") },
@@ -192,41 +180,70 @@ LRESULT CMyListCtrl1::CreateSingleGroup(int nIndex,
 	for (int i = 0; i < nItemCnt; ++i) {
 		CString& str0 = vec[i][0];
 		InsertItem(i, str0);
+		//lvItem.iGroup = i % (nItemCnt/4);//均分
+		//lvItem.iItem = i;
+		//lvItem.pszText = str0.GetBuffer();
+		//InsertItem(&lvItem);
 
 		for (int j = 1; j < len; ++j) {
 			CString& str = vec[i][j];
 			SetItemText(i, j, str);
 		}
 	}
-	
 
+	//组
 	EnableGroupView(TRUE);
 
 	LVGROUP lg = { 0 };
 	lg.cbSize = sizeof(lg);
-	lg.mask = LVGF_GROUPID | LVGF_HEADER | LVGF_STATE | LVGF_FOOTER | LVGF_ALIGN;
-	lg.iGroupId = nGroupId;
-	lg.pszHeader = strHeader.GetBuffer();
-	lg.cchHeader = strHeader.GetLength();
-	CString strFooter = _T("Group Footer");//只能一行
-	lg.pszFooter = strFooter.GetBuffer();
-	lg.cchFooter = strFooter.GetLength();
-	lg.uAlign = LVGA_FOOTER_CENTER;
-	lg.state = LVGS_COLLAPSIBLE ;
-	
-	nGroupId = InsertGroup(nIndex, &lg);//内部维护<索引,gid>
-	if (-1 == nGroupId)
-		return nGroupId;
+	lg.mask = LVGF_GROUPID | LVGF_HEADER | LVGF_STATE
+		| LVGF_TASK /*| LVGF_DESCRIPTIONTOP | LVGF_DESCRIPTIONBOTTOM*/ ;
+	lg.state = LVGS_COLLAPSIBLE;
 
-	for (int nRow = 0; nRow < GetItemCount(); ++nRow)
-	{
-		LVITEM lvItem = { 0 };
+	CString strFooter, strSubTitle, strTask;//只能一行
+	//CString strDesc = _T("description top");//只能一行
+	//CString strDescB = _T("description bottom");//只能一行
+	for (int i = 0; i < (int)m_vecGroupName.size(); ++i) {
+		lg.iGroupId = i;
+		CString& gname = m_vecGroupName[i];
+		lg.pszHeader = gname.GetBuffer();
+		lg.cchHeader = gname.GetLength();
+		
+		//strFooter.Format(_T("Group Footer:%s"), gname);
+		//lg.pszFooter = strFooter.GetBuffer();
+		//lg.cchFooter = strFooter.GetLength();
+
+		//strSubTitle.Format(_T("SubTitle:%s"), gname);
+		//lg.pszSubtitle = strSubTitle.GetBuffer();
+		//lg.cchSubtitle = strSubTitle.GetLength();
+
+		strTask.Format(_T("Task :%s"), gname);
+		lg.pszTask = strTask.GetBuffer();
+		lg.cchTask = strTask.GetLength();
+		//lg.pszDescriptionTop = strDesc.GetBuffer();
+		//lg.cchDescriptionTop = strDesc.GetLength();
+		//lg.pszDescriptionBottom = strDescB.GetBuffer();
+		//lg.cchDescriptionBottom= strDescB.GetLength();
+		//lg.uAlign = LVGA_FOOTER_CENTER;
+		//lg.iTitleImage = 1;  和文本不能重叠
+
+		nGroupId = InsertGroup(i, &lg);//内部维护<索引,gid>
+		if (-1 == nGroupId)
+			return nGroupId;
+
+		//绑定组：创建group就要绑定
+		memset(&lvItem, 0, sizeof(LVITEM));
 		lvItem.mask = LVIF_GROUPID;
-		lvItem.iItem = nRow;
-		//lvItem.iSubItem = 0;
-		lvItem.iGroupId = nGroupId;
-		SetItem(&lvItem);
+		int cnt = nItemCnt / 4;//每组绑定多少个
+		for (int nRow = nGroupId*cnt; nRow < (nGroupId+1)*cnt; ++nRow)
+		{
+			lvItem.iItem = nRow;
+			lvItem.iGroupId = nGroupId;
+			SetItem(&lvItem);
+		}
 	}
+
+
 
 	SetRedraw();
 
@@ -237,34 +254,62 @@ LRESULT CMyListCtrl1::CreateSingleGroup(int nIndex,
 
 
 
-void CMyListCtrl1::OnHdnTrack(NMHDR *pNMHDR, LRESULT *pResult)
+
+
+void CMyListCtrl1::OnLvnColumnclick(NMHDR *pNMHDR, LRESULT *pResult)
 {
-	LPNMHEADER phdr = reinterpret_cast<LPNMHEADER>(pNMHDR);
+	LPNMLISTVIEW pNMLV = reinterpret_cast<LPNMLISTVIEW>(pNMHDR);
 	// TODO: Add your control notification handler code here
-	HDITEM* p = phdr->pitem; //它并没有全部信息！
+	int nCol = pNMLV->iSubItem;
+	if (m_SortCol == nCol){
+		m_Ascending = !m_Ascending;
+	}
+	else{
+		m_preSortCol = m_SortCol;
+		m_SortCol = nCol;
+		m_Ascending = true;
+	}
 
-	//UINT mask = p->mask;
-	//mask = mask & HDI_TEXT; 为0！
+	//设置排序小箭头，去掉之前，加上现在
+	CHeaderCtrl* pHeader = GetHeaderCtrl();
+	HDITEM hditem = { 0 };
+	hditem.mask = HDI_FORMAT;
+	if (pHeader->GetItem(m_preSortCol, &hditem)) {
+		hditem.fmt &= ~(HDF_SORTDOWN | HDF_SORTUP);
+		pHeader->SetItem(m_preSortCol, &hditem);
+	}
+	if (pHeader->GetItem(m_SortCol, &hditem)) {
+		hditem.fmt &= ~(HDF_SORTDOWN | HDF_SORTUP);
+		hditem.fmt |= m_Ascending ? HDF_SORTUP : HDF_SORTDOWN;
+		pHeader->SetItem(m_SortCol, &hditem);
+	}
 
-	//获取item信息
-	//HDITEM item;
-	//item.mask = HDI_TEXT;
-	//TCHAR buf[MAX_PATH] = { 0 };
-	//item.pszText = buf;
-	//item.cchTextMax = MAX_PATH;
-	//CHeaderCtrl* pHead = GetHeaderCtrl();
-	//pHead->GetItem(phdr->iItem, &item);
-	//
-	//int minColWidth = 2 * GetStringWidth(item.pszText);
-	////int nowColWidth = GetColumnWidth(phdr->iItem);//和minColWidth一样！
-	//int nowColWidth = p->cxy;
-	//if (nowColWidth < minColWidth) {
-	//	SetColumnWidth(phdr->iItem, minColWidth);
-	//	pHead->EnableWindow(FALSE);
-	//}
+	//PARAMSORT paramsort(this, nCol, m_Ascending, g_sortType[nCol]);
+	//ListView_SortItemsEx(GetSafeHwnd(), SortFunc, &paramsort);
+	SortItemsEx(SortFunc, (DWORD_PTR)this);
 
 	*pResult = 0;
 }
 
 
+int CALLBACK SortFunc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort)
+{
+	//PARAMSORT& ps = *(PARAMSORT*)lParamSort;
+	CMyListCtrl1& pl = *((CMyListCtrl1*)lParamSort);
 
+	const CString& left = pl.GetItemText(lParam1, pl.m_SortCol);
+	const CString& right = pl.GetItemText(lParam2, pl.m_SortCol);
+
+	int a, b;
+	switch (pl.m_vecSortType[pl.m_SortCol])
+	{
+	case CMyListCtrl1::STRING:
+		return pl.m_Ascending ? _tcscmp(left, right) : _tcscmp(right, left);
+	case CMyListCtrl1::INTEGER:
+	case CMyListCtrl1::PERCENTAGE:
+		a = _ttoi(left); b = _ttoi(right);
+		return pl.m_Ascending ? a > b : b > a;
+	}
+
+	return 1;
+}
--- owndraw_ctrl/listctrl_test1.h
+++ owndraw_ctrl/listctrl_test1.h
@@ -1,31 +1,54 @@
 #pragma once
 #include "utility.h"
+#include <vector>
 
 class CMyListCtrl1 : public CListCtrl, public CUICfg
 {
-	//方法
 	DECLARE_MESSAGE_MAP()
+	friend int CALLBACK SortFunc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort);
+
 public:
 	CMyListCtrl1();
 	bool Init();
 
-protected:
-private:
-	//属性
-public:
-protected:
-	CImageList				m_imgList;
-	CImageList				m_imgListSmall;
-	CImageList				m_imgListState;
+	afx_msg void OnLvnEndlabeledit(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnLvnColumnclick(NMHDR *pNMHDR, LRESULT *pResult);
 
+protected:
 private:
 	virtual bool regex_callback(const boost::smatch& what);//ui正则回调
-	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 	virtual void PreSubclassWindow();
 
-	LRESULT	CreateSingleGroup(int nIndex, int nGroupId, CString strHeader);
+	LRESULT	CreateGroup(int nIndex, int nGroupId, CString strHeader);
+
+
+
+
+
 public:
-	afx_msg void OnLvnEndlabeledit(NMHDR *pNMHDR, LRESULT *pResult);
-	afx_msg void MeasureItem(/*int nIDCtl, */LPMEASUREITEMSTRUCT lpMeasureItemStruct);
-	afx_msg void OnHdnTrack(NMHDR *pNMHDR, LRESULT *pResult);
+	enum SORTTYPE {//列排序类型
+		STRING,//字符串
+		INTEGER,//整数
+		PERCENTAGE//百分比
+	};
+
+
+protected:
+	CImageList				m_imgList;//32px图标
+	CImageList				m_imgListSmall;//16px图标
+	CImageList				m_imgListState;//state图标
+	CImageList				m_imgListGroup;//group图标
+
+private:
+	int m_SortCol = -1;//当前选择的列
+	int m_preSortCol = -1;//之前选择的列
+	bool m_Ascending = true;//升序
+
+	std::vector<CString>	m_vecColsName //列名
+		= { _T("姓名"),_T("年龄"), _T("性别"), _T("爱好"), _T("工作进度") };
+	std::vector<SORTTYPE>	m_vecSortType//列排序类型
+		= { STRING, INTEGER, STRING, STRING, PERCENTAGE };
+	std::vector<CString>	m_vecGroupName
+		= { _T("甲组"),_T("乙组"), _T("丙组"), _T("丁组") };
+
 };
